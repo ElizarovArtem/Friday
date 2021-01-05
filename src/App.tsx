70e7d4e82a3219288d727d77c4836c4fbddc078f
ElizarovArtem <elizarovskiyklach@yandex.ru>
2021-01-05 22:24:58 +0300

    add pg-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Redirect, Route, Switch } from 'react-router-dom';
+import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import './App.css';
 import {Login} from "./Components/Login/Login";
 import {Registration} from "./Components/Registration/Registration";
@@ -10,20 +10,22 @@ import {Test} from "./Components/Test/Test";
 import {Error} from "./Components/Error/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path={'/login'} render={() => <Login/>}/>
-        <Route path={'/registration'} render={() => <Registration/>}/>
-        <Route path={'/profile'} render={() => <Profile/>}/>
-        <Route path={'/pas-recovery'} render={() => <PasswordRecovery/>}/>
-        <Route path={'/pas-enter'} render={() => <NewPasswordEnter/>}/>
-        <Route path={'/test'} render={() => <Test/>}/>
-        <Route path={'/404'} render={() => <Error/>}/>
-        <Redirect from={'*'} to={'/404'}/>
-      </Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HashRouter>
+                <Switch>
+                    <Route path={'/login'} render={() => <Login/>}/>
+                    <Route path={'/registration'} render={() => <Registration/>}/>
+                    <Route path={'/profile'} render={() => <Profile/>}/>
+                    <Route path={'/pas-recovery'} render={() => <PasswordRecovery/>}/>
+                    <Route path={'/pas-enter'} render={() => <NewPasswordEnter/>}/>
+                    <Route path={'/test'} render={() => <Test/>}/>
+                    <Route path={'/404'} render={() => <Error/>}/>
+                    <Redirect from={'*'} to={'/404'}/>
+                </Switch>
+            </HashRouter>
+        </div>
+    );
 }
 
 export default App;
