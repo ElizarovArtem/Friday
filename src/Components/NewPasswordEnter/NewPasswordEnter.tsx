fd3efa91e10b603cd26320142fd952e6e4c9e5fd
AlexB <7894664@mail.ru>
2021-01-10 21:31:54 +0300

    create inputFields to PasswordRecovery.tsx

--- src/Components/NewPasswordEnter/NewPasswordEnter.tsx
+++ src/Components/NewPasswordEnter/NewPasswordEnter.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-
 export const NewPasswordEnter = () => {
-    return (
-        <div>
-            <h1>NewPasswordEnter</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>NewPasswordEnter</h1>
+      <div>
+        <label htmlFor="email">Enter new password</label>
+        <input id="email" type="password" />
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
